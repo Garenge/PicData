5e90c237d442c6c82f5c9a1223644c94e623d10c
Garenge <garenge@outlook.com>
2026-06-16 00:04:09 +0800

    pref: add export share dependencies

--- PicData-Flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ PicData-Flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import screen_retriever_macos
+import share_plus
 import shared_preferences_foundation
 import sqflite_darwin
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
